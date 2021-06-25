037916e9f486b322c89cd2bcef22741391889cdf
Jeganathan Swaminathan <mail2jegan@gmail.com>
2021-06-25 00:06:37 -0700

    Added instructions to copy settings.xml in Docker image.

---
+++ Day10/DevOpsPipeline/Hello/src/main/java/org/tektutor/App.java
@@ -0,0 +1,13 @@
+package org.tektutor;
+
+public class App {
+
+	public String sayHello() {
+		return "Hello Jenkins!";
+	}
+
+	public static void main ( String[] args ) {
+		App app = new App();
+		System.out.println ( app.sayHello() );
+	}
+}
